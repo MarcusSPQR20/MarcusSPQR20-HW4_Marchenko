71ab7a0a787dc2f8bd9f453e57b85f540ca5ea94
Marcus <MarcusSPQR20@gmail.com>
2019-12-06 11:58:57 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider-carousel').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        
+    });
+});
+
